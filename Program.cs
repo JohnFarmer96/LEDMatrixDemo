19b696072cc2191bdd0e18dc79dad87c6519bc2f
Jonathan Bauer <jonathan.bauer96@googlemail.com>
2021-02-01 16:08:20 +0100

    Debug changes.

--- Program.cs
+++ Program.cs
@@ -31,23 +31,24 @@ namespace LEDMatrixDemo
             var canvas = matrix.CreateOffscreenCanvas();
 
             Console.WriteLine("Starting Visualization");
+            canvas.Fill(new Color(100, 100, 100));
 
-            for (var i = 0; i < 1000; ++i)
-            {
-                canvas.Fill(new Color(255, 255, 255));
-                //for (var y = 0; y < canvas.Height; ++y)
-                //{
-                //    for (var x = 0; x < canvas.Width; ++x)
-                //    {
-                //        canvas.SetPixel(x, y, new Color(i & 0xff, x, y));
-                //    }
-                //}
-                //canvas.DrawCircle(canvas.Width / 2, canvas.Height / 2, 6, new Color(0, 0, 255));
-                //canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 - 3, canvas.Width / 2 + 3, canvas.Height / 2 + 3, new Color(0, 0, 255));
-                //canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 + 3, canvas.Width / 2 + 3, canvas.Height / 2 - 3, new Color(0, 0, 255));
+            //for (var i = 0; i < 1000; ++i)
+            //{
+            //    canvas.Fill(new Color(100, 100, 100));
+            //    for (var y = 0; y < canvas.Height; ++y)
+            //    {
+            //        for (var x = 0; x < canvas.Width; ++x)
+            //        {
+            //            canvas.SetPixel(x, y, new Color(i & 0xff, x, y));
+            //        }
+            //    }
+            //    canvas.DrawCircle(canvas.Width / 2, canvas.Height / 2, 6, new Color(0, 0, 255));
+            //    canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 - 3, canvas.Width / 2 + 3, canvas.Height / 2 + 3, new Color(0, 0, 255));
+            //    canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 + 3, canvas.Width / 2 + 3, canvas.Height / 2 - 3, new Color(0, 0, 255));
 
-                canvas = matrix.SwapOnVsync(canvas);
-            }
+            //    canvas = matrix.SwapOnVsync(canvas);
+            //}
 
             Console.WriteLine("Done.");
             return 0;
