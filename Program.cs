0b444543333fb763b2c8762fa2fd13519927e4cd
Jonathan Bauer <jonathan.bauer96@googlemail.com>
2021-02-01 16:29:33 +0100

    Program commit after reset

--- Program.cs
+++ Program.cs
@@ -22,8 +22,7 @@ namespace LEDMatrixDemo
                 Cols = 64,
                 GpioSlowdown = 4,
                 Brightness = 100,
-                ChainLength = 1, 
-                LimitRefreshRateHz = 60
+                ChainLength = 1
             };
 
             Console.WriteLine("Starting to initialize Matrix");
@@ -31,26 +30,26 @@ namespace LEDMatrixDemo
             var canvas = matrix.CreateOffscreenCanvas();
 
             Console.WriteLine("Starting Visualization");
-            canvas.SetPixel(0, 0, new Color(100, 100, 100));
 
-            //for (var i = 0; i < 1000; ++i)
-            //{
-            //    canvas.Fill(new Color(100, 100, 100));
-            //    for (var y = 0; y < canvas.Height; ++y)
-            //    {
-            //        for (var x = 0; x < canvas.Width; ++x)
-            //        {
-            //            canvas.SetPixel(x, y, new Color(i & 0xff, x, y));
-            //        }
-            //    }
-            //    canvas.DrawCircle(canvas.Width / 2, canvas.Height / 2, 6, new Color(0, 0, 255));
-            //    canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 - 3, canvas.Width / 2 + 3, canvas.Height / 2 + 3, new Color(0, 0, 255));
-            //    canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 + 3, canvas.Width / 2 + 3, canvas.Height / 2 - 3, new Color(0, 0, 255));
+            for (var i = 0; i < 1000; ++i)
+            {
+                canvas.Fill(new Color(100, 100, 100));
+                for (var y = 0; y < canvas.Height; ++y)
+                {
+                    for (var x = 0; x < canvas.Width; ++x)
+                    {
+                        canvas.SetPixel(x, y, new Color(i & 0xff, x, y));
+                    }
+                }
+                canvas.DrawCircle(canvas.Width / 2, canvas.Height / 2, 6, new Color(0, 0, 255));
+                canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 - 3, canvas.Width / 2 + 3, canvas.Height / 2 + 3, new Color(0, 0, 255));
+                canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 + 3, canvas.Width / 2 + 3, canvas.Height / 2 - 3, new Color(0, 0, 255));
 
-            //    canvas = matrix.SwapOnVsync(canvas);
-            //}
+                canvas = matrix.SwapOnVsync(canvas);
+            }
 
             Console.WriteLine("Done.");
+            Console.ReadKey();
             return 0;
         }
     }
